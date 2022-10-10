149511972ce429313266be6f68a5a079bc82924f
Luise Koehler <luise.koehler@bbaw.de>
2022-10-10 11:50:52 +0200

    Remove duplicate elements form individual header

--- tei_make_corpus/header_handler.py
+++ tei_make_corpus/header_handler.py
@@ -1,7 +1,13 @@
+from typing import Optional
+
 from lxml import etree
+
+from tei_make_corpus.element_equality import elements_equal
 
 
 class TeiHeaderHandler:
+    tags_no_leftover_sibling = {"distributor", "publisher", "authority"}
+
     def __init__(self, header_file_path: str) -> None:
         self._header_file = header_file_path
         self._cheader = self._construct_common_header(header_file_path)
@@ -10,7 +16,29 @@ class TeiHeaderHandler:
         return self._cheader
 
     def declutter_individual_header(self, iheader: etree._Element) -> None:
-        pass
+        for element in self._cheader.iterdescendants():
+            if etree.QName(element.tag).localname in self.tags_no_leftover_sibling:
+                continue
+            matching = self._element_equivalent_in_individual_header(element, iheader)
+            if matching is not None:
+                if elements_equal(element, matching, ignore_ns=True):
+                    matching.getparent().remove(matching)
+        for element in self._cheader.iterdescendants(self.tags_no_leftover_sibling):
+            matching = self._element_equivalent_in_individual_header(element, iheader)
+            if matching is not None:
+                if elements_equal(element, matching, ignore_ns=True):
+                    if matching.getnext() is None:
+                        matching.getparent().replace(matching, etree.Element("p"))
 
     def _construct_common_header(self, header_file: str) -> etree._Element:
         return etree.parse(header_file).getroot()
+
+    def _element_equivalent_in_individual_header(
+        self, element: etree._Element, iheader: etree._Element
+    ) -> Optional[etree._Element]:
+        cheader_tree = self._cheader.getroottree()
+        elem_xpath = cheader_tree.getelementpath(element)
+        matching = iheader.find(
+            "./" + elem_xpath, namespaces={None: "http://www.tei-c.org/ns/1.0"}
+        )
+        return matching
--- tests/test_header_handler.py
+++ tests/test_header_handler.py
@@ -1,10 +1,11 @@
+import io
 import os
 import unittest
 
 from lxml import etree
 
+from tei_make_corpus.element_equality import elements_equal
 from tei_make_corpus.header_handler import TeiHeaderHandler
-from tests.utils import elements_equal
 
 
 class TeiHeaderHandlerTest(unittest.TestCase):
@@ -37,5 +38,192 @@ class TeiHeaderHandlerTest(unittest.TestCase):
         result = header_handler.common_header()
         self.assertXmlElementsEqual(result, expected)
 
-    def assertXmlElementsEqual(self, elem1, elem2):
-        return elements_equal(elem1, elem2)
+    def test_simple_element_from_individual_header_removed_if_in_common_header(self):
+        header_file = os.path.join(self.testdata, "corpus_header", "header.xml")
+        header_handler = TeiHeaderHandler(header_file)
+        tei_doc = etree.parse(os.path.join(self.testdata, "corpus_header", "file1.xml"))
+        iheader = tei_doc.find(".//{*}teiHeader")
+        header_handler.declutter_individual_header(iheader)
+        self.assertEqual(tei_doc.findall(".//{*}funder"), [])
+
+    def test_simple_element_not_removed_if_different_in_common_header(self):
+        header_file = os.path.join(self.testdata, "corpus_header", "header.xml")
+        header_handler = TeiHeaderHandler(header_file)
+        tei_doc = etree.parse(os.path.join(self.testdata, "corpus_header", "file1.xml"))
+        iheader = tei_doc.find(".//{*}teiHeader")
+        header_handler.declutter_individual_header(iheader)
+        self.assertTrue(tei_doc.findall(".//{*}titleStmt/{*}author") != [])
+
+    def test_simple_element_not_removed_if_attributes_different(self):
+        header_file = os.path.join(self.testdata, "corpus_header", "header.xml")
+        header_handler = TeiHeaderHandler(header_file)
+        tei_doc = etree.parse(os.path.join(self.testdata, "corpus_header", "file1.xml"))
+        iheader = tei_doc.find(".//{*}teiHeader")
+        header_handler.declutter_individual_header(iheader)
+        self.assertTrue(tei_doc.findall(".//{*}publicationStmt/{*}availability") != [])
+
+    def test_simple_element_removed_if_attributes_also_match(self):
+        header_file = os.path.join(self.testdata, "corpus_header", "header.xml")
+        header_handler = TeiHeaderHandler(header_file)
+        tei_doc = etree.XML(
+            """
+        <TEI xmlns="http://www.tei-c.org/ns/1.0">
+            <teiHeader>
+                <fileDesc>
+                    <publicationStmt>
+                        <pubPlace>place</pubPlace>
+                        <availability n="test" status="unknown">Some text</availability>
+                    </publicationStmt>
+                </fileDesc>
+            </teiHeader>
+        </TEI>
+        """
+        )
+        iheader = tei_doc.find(".//{*}teiHeader")
+        header_handler.declutter_individual_header(iheader)
+        self.assertEqual(iheader.findall(".//{*}availability"), [])
+
+    def test_element_publisher_not_removed_from_iheader_publicationStmt_if_siblings(
+        self,
+    ):
+        header_file = os.path.join(self.testdata, "corpus_header", "header.xml")
+        header_handler = TeiHeaderHandler(header_file)
+        tei_doc = etree.XML(
+            """<TEI xmlns="http://www.tei-c.org/ns/1.0">
+            <teiHeader>
+            <fileDesc>
+              <titleStmt/>
+              <publicationStmt>
+                <publisher>Publisher</publisher>
+                <pubPlace>some place</pubPlace>
+                <availability n="test" status="unknown">Some text</availability>
+              </publicationStmt>
+            </fileDesc>
+          </teiHeader>
+          </TEI>"""
+        )
+        iheader = tei_doc.find(".//{*}teiHeader")
+        header_handler.declutter_individual_header(iheader)
+        self.assertTrue(iheader.findall(".//{*}publisher") != [])
+
+    def test_element_authority_not_removed_from_iheader_publicationStmt_if_siblings(
+        self,
+    ):
+        header_file = io.BytesIO(
+            b"""<teiHeader>
+            <fileDesc>
+                <titleStmt>
+                    <title>Corpus title</title>
+                </titleStmt>
+                <publicationStmt>
+                  <authority>Publisher</authority>
+                  <pubPlace>Place</pubPlace>
+                  <date>today</date>
+                </publicationStmt>
+                <sourceDesc>
+                  <p/>
+                </sourceDesc>
+            </fileDesc>
+        </teiHeader>"""
+        )
+        header_handler = TeiHeaderHandler(header_file)
+        tei_doc = etree.XML(
+            """<TEI xmlns="http://www.tei-c.org/ns/1.0">
+            <teiHeader>
+            <fileDesc>
+              <titleStmt/>
+              <publicationStmt>
+                <authority>Publisher</authority>
+                <pubPlace>some place</pubPlace>
+                <availability n="test" status="unknown">Some text</availability>
+              </publicationStmt>
+            </fileDesc>
+          </teiHeader>
+          </TEI>"""
+        )
+        iheader = tei_doc.find(".//{*}teiHeader")
+        header_handler.declutter_individual_header(iheader)
+        self.assertTrue(iheader.findall(".//{*}authority") != [])
+
+    def test_element_distributor_not_removed_from_iheader_publicationStmt_if_siblings(
+        self,
+    ):
+        header_file = io.BytesIO(
+            b"""<teiHeader>
+            <fileDesc>
+                <titleStmt>
+                    <title>Corpus title</title>
+                </titleStmt>
+                <publicationStmt>
+                  <distributor>Publisher</distributor>
+                  <pubPlace>Place</pubPlace>
+                  <date>today</date>
+                </publicationStmt>
+                <sourceDesc>
+                  <p/>
+                </sourceDesc>
+            </fileDesc>
+        </teiHeader>"""
+        )
+        header_handler = TeiHeaderHandler(header_file)
+        tei_doc = etree.XML(
+            """<TEI xmlns="http://www.tei-c.org/ns/1.0">
+            <teiHeader>
+            <fileDesc>
+              <titleStmt/>
+              <publicationStmt>
+                <distributor>Publisher</distributor>
+                <pubPlace>some place</pubPlace>
+                <availability n="test" status="unknown">Some text</availability>
+              </publicationStmt>
+            </fileDesc>
+          </teiHeader>
+          </TEI>"""
+        )
+        iheader = tei_doc.find(".//{*}teiHeader")
+        header_handler.declutter_individual_header(iheader)
+        self.assertTrue(iheader.findall(".//{*}distributor") != [])
+
+    def test_elements_in_publicationStmt_replaced_by_p_if_removed(self):
+        header_file = io.BytesIO(
+            b"""<teiHeader>
+            <fileDesc>
+                <titleStmt>
+                    <title>Corpus title</title>
+                </titleStmt>
+                <publicationStmt>
+                  <distributor>Publisher</distributor>
+                  <pubPlace>Place</pubPlace>
+                  <date>today</date>
+                </publicationStmt>
+                <sourceDesc>
+                  <p/>
+                </sourceDesc>
+            </fileDesc>
+        </teiHeader>"""
+        )
+        header_handler = TeiHeaderHandler(header_file)
+        tei_doc = etree.XML(
+            """<TEI xmlns="http://www.tei-c.org/ns/1.0">
+            <teiHeader>
+            <fileDesc>
+              <titleStmt/>
+              <publicationStmt>
+                <distributor>Publisher</distributor>
+              </publicationStmt>
+            </fileDesc>
+          </teiHeader>
+          </TEI>"""
+        )
+        iheader = tei_doc.find(".//{*}teiHeader")
+        header_handler.declutter_individual_header(iheader)
+        result = [
+            child.tag for child in iheader.find(".//{*}publicationStmt").getchildren()
+        ]
+        self.assertEqual(result, ["p"])
+
+    def test_complex_element_removed_from_individual_header_if_in_common_header(self):
+        pass
+
+    def assertXmlElementsEqual(self, element1, element2):
+        self.assertTrue(elements_equal(element1, element2))
