67056b5f38d49ef865f84ec52578ce67fc716481
Luise Koehler <luise.koehler@bbaw.de>
2022-10-10 15:32:55 +0200

    Implement cli controller

--- tei_make_corpus/cli/controller.py
+++ tei_make_corpus/cli/controller.py
@@ -1,6 +1,7 @@
 import argparse
+from typing import List
 
-from tei_make_corpus.cli.tei_make_corpus_use_case import TeiMakeCorpusUseCase
+from tei_make_corpus.cli.make_corpus_usecase import CliRequest, TeiMakeCorpusUseCase
 
 
 class TeiMakeCorpusController:
@@ -10,3 +11,21 @@ class TeiMakeCorpusController:
 
     def __init__(self, use_case: TeiMakeCorpusUseCase) -> None:
         self.use_case = use_case
+
+    def process_arguments(self, arguments: List[str]) -> None:
+        parser = argparse.ArgumentParser(
+            description="Create a *teiCorpus* file from a collection of TEI documents. "
+        )
+        parser.add_argument(
+            "corpus_dir", help="Directory containing the TEI files.", type=str
+        )
+        parser.add_argument(
+            "--cheader",
+            "-c",
+            help="Xml file containing the common header for the whole corpus.",
+            required=True,
+        )
+        args = parser.parse_args(arguments)
+        self.use_case.process(
+            CliRequest(header_file=args.cheader, corpus_dir=args.corpus_dir)
+        )
