8044f293f5728c478b11335bced442c261edd529
Luise Koehler <luise.koehler@bbaw.de>
2022-10-11 15:32:25 +0200

    Add logging

--- tei_make_corpus/cli/cli_interface.py
+++ tei_make_corpus/cli/cli_interface.py
@@ -1,7 +1,15 @@
+import logging
 import sys
 
 from tei_make_corpus.cli.controller import TeiMakeCorpusController
 from tei_make_corpus.cli.make_corpus_usecase import TeiMakeCorpusUseCaseImpl
+
+logging.basicConfig(
+    filename="tei-make-corpus.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(name)s: %(message)s",
+)
+logger = logging.getLogger()
 
 
 def main() -> None:
