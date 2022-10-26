ba0b2f1e3fa68ee50d0efb5556d0f0fe0e3fdfad
Luise Koehler <luise.koehler@bbaw.de>
2022-10-26 15:35:40 +0200

    Implent partitioning for number of files per doc

--- tei_make_corpus/partitioner.py
+++ tei_make_corpus/partitioner.py
@@ -1,45 +1,53 @@
-import os
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import Generator, List, Optional, Tuple
 
 from tei_make_corpus.cli.corpus_config import CorpusConfig
 from tei_make_corpus.header_handler import TeiHeaderHandler
 from tei_make_corpus.partition import Partition
+from tei_make_corpus.path_finder import PathFinder
 
 
 @dataclass
 class Partitioner:
     header_handler: TeiHeaderHandler
+    path_finder: PathFinder
 
     def get_partitions(
         self, corpus_dir: str, header_file: str, config: Optional[CorpusConfig] = None
-    ) -> List[Partition]:
-        # get info about split
+    ) -> Generator[Partition, None, None]:
         clean = False
+        docs_per_file = -1
         if config is not None:
             clean = config.clean_header
-        return self._determine_partitions(corpus_dir, header_file, clean_files=clean)
+            docs_per_file = config.split_docs
+        return self._determine_partitions(
+            corpus_dir, header_file, clean_files=clean, docs_per_file=docs_per_file
+        )
 
     def _determine_partitions(
-        self, corpus_dir: str, header_file: str, clean_files: bool = False
-    ) -> List[Partition]:
-        # split
-        return [
-            Partition(
+        self,
+        corpus_dir: str,
+        header_file: str,
+        clean_files: bool = False,
+        docs_per_file: int = -1,
+    ) -> Generator[Partition, None, None]:
+        all_files = self.path_finder.get_paths_for_corpus_files(corpus_dir, header_file)
+        total_number_files = len(all_files)
+        for start_index, end_index in self._determine_chunk_indices(
+            total_number_files, docs_per_file
+        ):
+            yield Partition(
                 self.header_handler,
-                self._get_paths_for_corpus_files(corpus_dir, header_file),
+                all_files[start_index:end_index],
                 clean_files=clean_files,
             )
-        ]
 
-    def _get_paths_for_corpus_files(
-        self, corpus_dir: str, header_file: str
-    ) -> List[str]:
-        return sorted(
-            (
-                os.path.join(root, file)
-                for root, dirs, files in os.walk(corpus_dir)
-                for file in files
-                if file != os.path.basename(header_file) and file.endswith(".xml")
-            )
-        )
+    def _determine_chunk_indices(
+        self, total_num_of_files: int, intended_chunk_size: int
+    ) -> List[Tuple[int, int]]:
+        if intended_chunk_size == -1 or total_num_of_files < intended_chunk_size:
+            return [(0, total_num_of_files)]
+        return [
+            (i, i + intended_chunk_size)
+            for i in range(0, total_num_of_files, intended_chunk_size)
+        ]
--- tests/test_partitioner.py
+++ tests/test_partitioner.py
@@ -1,82 +1,85 @@
 import os
-import tempfile
+import random
 import unittest
 
+from tei_make_corpus.cli.corpus_config import CorpusConfig
 from tei_make_corpus.partitioner import Partitioner
 from tests.utils import MockHeaderHandler
+
+
+class MockPathFinder:
+    def __init__(self):
+        self.files = dict()
+
+    def get_paths_for_corpus_files(self, corpus_dir, header_file):
+        return self.files.get(corpus_dir, [])
 
 
 class PartitionerTest(unittest.TestCase):
     def setUp(self):
         self.mock_header_handler = MockHeaderHandler()
-        self.partitioner = Partitioner(self.mock_header_handler)
-
-    def test_header_file_ignored_if_in_corpus_directory(self):
-        corpus_dir = os.path.join("tests", "testdata", "corpus")
-        header_file = "header.xml"
-        corpus_files = self.partitioner._get_paths_for_corpus_files(
-            corpus_dir, header_file
-        )
-        expected = [
-            "tests/testdata/corpus/file1.xml",
-            "tests/testdata/corpus/file2.xml",
-        ]
-        self.assertEqual(list(corpus_files), expected)
-
-    def test_header_file_ignored_if_in_corpus_directory_with_path(self):
-        corpus_dir = os.path.join("tests", "testdata", "corpus")
-        header_file = os.path.join("tests", "testdata", "corpus", "header.xml")
-        corpus_files = self.partitioner._get_paths_for_corpus_files(
-            corpus_dir, header_file
-        )
-        expected = [
-            "tests/testdata/corpus/file1.xml",
-            "tests/testdata/corpus/file2.xml",
-        ]
-        self.assertEqual(list(corpus_files), expected)
-
-    def test_non_xml_files_ignored_in_corpus_directory(self):
-        corpus_dir = os.path.join("tests", "testdata", "corpus")
-        header_file = "header.xml"
-        corpus_files = self.partitioner._get_paths_for_corpus_files(
-            corpus_dir, header_file
-        )
-        expected = [
-            "tests/testdata/corpus/file1.xml",
-            "tests/testdata/corpus/file2.xml",
-        ]
-        self.assertEqual(list(corpus_files), expected)
-
-    def test_corpus_files_found_recursively(self):
-        corpus_dir = os.path.join("tests", "testdata", "rec_corpus")
-        header_file = "header.xml"
-        corpus_files = self.partitioner._get_paths_for_corpus_files(
-            corpus_dir, header_file
-        )
-        expected = [
-            "tests/testdata/rec_corpus/part1/subpart/file1.xml",
-            "tests/testdata/rec_corpus/part1/subpart/file2.xml",
-            "tests/testdata/rec_corpus/part2/subpart/file21.xml",
-            "tests/testdata/rec_corpus/part2/subpart/file22.xml",
-        ]
-        self.assertEqual(list(corpus_files), expected)
-
-    def test_files_ordered_alphabetically(self):
-        header_file = "header.xml"
-        with tempfile.TemporaryDirectory() as tempdir:
-            file_names = []
-            for _ in range(10):
-                sub_dir = tempfile.mkdtemp(dir=tempdir)
-                for _ in range(100):
-                    _, tmp = tempfile.mkstemp(".xml", dir=sub_dir, text=True)
-                    file_names.append(tmp)
-            corpus_files = self.partitioner._get_paths_for_corpus_files(
-                tempdir, header_file
-            )
-        self.assertEqual(corpus_files, sorted(file_names))
+        self.mock_path_finder = MockPathFinder()
+        self.partitioner = Partitioner(self.mock_header_handler, self.mock_path_finder)
 
     def test_only_one_partition_returned_in_default_setting(self):
         corpus_dir = os.path.join("tests", "testdata", "rec_corpus")
         header_file = os.path.join("tests", "testdata", "header.xml")
-        result = self.partitioner.get_partitions(corpus_dir, header_file)
+        corpus_files = [
+            os.path.join(root, file)
+            for root, dirs, files in os.walk(corpus_dir)
+            for file in files
+        ]
+        self.mock_path_finder.files[corpus_dir] = corpus_files
+        result = list(self.partitioner.get_partitions(corpus_dir, header_file))
         self.assertEqual(len(result), 1)
+
+    def test_partitioning_with_number_of_documents_per_file(self):
+        header_file = "header.xml"
+        total_no_files = 1000
+        split_val = 100
+        corpus_files = ["file.xml"] * total_no_files
+        self.mock_path_finder.files["test_dir"] = corpus_files
+        config = CorpusConfig(clean_header=False, split_docs=split_val)
+        partitions = list(
+            self.partitioner.get_partitions("test_dir", header_file, config)
+        )
+        self.assertEqual(len(partitions), 10)
+        self.assertEqual(sum(len(part) for part in partitions), total_no_files)
+
+    def test_partitioning_with_random_number_of_documents_per_file(self):
+        header_file = "header.xml"
+        total_no_files = random.randint(10, 15_000_000)
+        split_val = random.randint(1, total_no_files)
+        corpus_dir = "test_dir"
+        corpus_files = [f"file{i}.xml" for i in range(total_no_files)]
+        config = CorpusConfig(clean_header=False, split_docs=split_val)
+        self.mock_path_finder.files[corpus_dir] = corpus_files
+        partitions = self.partitioner.get_partitions(corpus_dir, header_file, config)
+        self.assertEqual(sum(len(part) for part in partitions), total_no_files)
+
+    def test_partitioning_split_value_larger_than_total_returns_one_partition(self):
+        header_file = "header.xml"
+        total_no_files = random.randint(10, 15_000)
+        split_val = random.randint(total_no_files, 15_000_000)
+        corpus_dir = "test_dir"
+        corpus_files = [f"file{i}.xml" for i in range(total_no_files)]
+        self.mock_path_finder.files[corpus_dir] = corpus_files
+        config = CorpusConfig(clean_header=False, split_docs=split_val)
+        partitions = list(
+            self.partitioner.get_partitions(corpus_dir, header_file, config)
+        )
+        self.assertEqual(len(partitions), 1)
+
+    def test_all_files_contained_in_partitions(self):
+        header_file = "header.xml"
+        total_no_files = random.randint(100, 10_000)
+        split_val = random.randint(10, total_no_files)
+        corpus_dir = "corpus"
+        corpus_files = [f"file{i}.xml" for i in range(total_no_files)]
+        self.mock_path_finder.files[corpus_dir] = corpus_files
+        config = CorpusConfig(clean_header=False, split_docs=split_val)
+        partitions = self.partitioner.get_partitions(corpus_dir, header_file, config)
+        result = []
+        for part in partitions:
+            result += part.files
+        self.assertEqual(result, corpus_files)
