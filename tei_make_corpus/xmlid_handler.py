e03166a1a8a4cf0956f8a3e92d6aec2f05cefe4a
Luise Koehler <luise.koehler@bbaw.de>
2023-06-08 16:53:11 +0200

    Add prefix to xml:id

--- tei_make_corpus/xmlid_handler.py
+++ tei_make_corpus/xmlid_handler.py
@@ -1,17 +1,65 @@
-from typing import Protocol
+import uuid
+from typing import Optional, Protocol, Set
 
 from lxml import etree
 
 
 class XmlIdHandler(Protocol):
-    def process_document(self, doc_root: etree._Element) -> None:
+    def process_document(self, doc_root: etree._Element, filepath: str) -> None:
         ...
 
 
 class XmlIdHandlerImpl:
-    def process_document(self, doc_root: etree._Element) -> None:
-        self._remove_all_xmlid_attributes(doc_root)
+    def __init__(self, action: Optional[str] = None) -> None:
+        self.action = action
+        self._prefixes: Set[str] = set()
+
+    def process_document(self, doc_root: etree._Element, filepath: str) -> None:
+        if self.action is None:
+            self._remove_all_xmlid_attributes(doc_root)
+        if self.action == "prefix":
+            self._add_prefix_to_xmlid_attributes(doc_root, filepath)
+
+    def generate_prefix(self, filepath: str) -> str:
+        uid = uuid.uuid5(uuid.NAMESPACE_DNS, filepath).hex
+        prefix = uid[:6]
+        tmp_prefix = prefix
+        suffix_on_collision = 0
+        while tmp_prefix in self._prefixes:
+            tmp_prefix = f"{prefix}{suffix_on_collision}"
+            suffix_on_collision += 1
+        self._prefixes.add(tmp_prefix)
+        return f"p{tmp_prefix}"
 
     def _remove_all_xmlid_attributes(self, tei_doc: etree._Element) -> None:
         for node in tei_doc.findall(".//*[@{http://www.w3.org/XML/1998/namespace}id]"):
             node.attrib.pop("{http://www.w3.org/XML/1998/namespace}id")
+
+    def _add_prefix_to_xmlid_attributes(
+        self, doc_root: etree._Element, filepath: str
+    ) -> None:
+        prefix = self.generate_prefix(filepath)
+        if (
+            xmlid_attrib := doc_root.get("{http://www.w3.org/XML/1998/namespace}id")
+        ) is not None:
+            doc_root.set(
+                "{http://www.w3.org/XML/1998/namespace}id",
+                f"{prefix}-{xmlid_attrib}",
+            )
+            self._add_prefix_to_referencing_attributes(doc_root, xmlid_attrib, prefix)
+
+        for node in doc_root.findall(".//*[@{http://www.w3.org/XML/1998/namespace}id]"):
+            xmlid_attrib = node.get("{http://www.w3.org/XML/1998/namespace}id")
+            node.set(
+                "{http://www.w3.org/XML/1998/namespace}id",
+                f"{prefix}-{xmlid_attrib}",
+            )
+            self._add_prefix_to_referencing_attributes(doc_root, xmlid_attrib, prefix)
+
+    def _add_prefix_to_referencing_attributes(
+        self, doc_root: etree._Element, xmlid_value: str, prefix: str
+    ) -> None:
+        for element in doc_root.xpath(f"//*[@*= '#{xmlid_value}']"):
+            for attrib, value in element.items():
+                if value == "#" + xmlid_value:
+                    element.set(attrib, f"#{prefix}-{xmlid_value}")
--- tests/test_xmlid_handler.py
+++ tests/test_xmlid_handler.py
@@ -7,10 +7,215 @@ from tei_make_corpus.xmlid_handler import XmlIdHandlerImpl
 
 class XmlIdHandlerImplTest(unittest.TestCase):
     def setUp(self):
-        self.xmlid_handler = XmlIdHandlerImpl()
+        self.default_handler = XmlIdHandlerImpl()
+        self.prefix_handler = XmlIdHandlerImpl(action="prefix")
 
     def test_remove_xmlid_attribute_from_element(self):
-        doc = etree.fromstring("<root><one xml:id='a'><two xml:id='b'/></one></root>")
-        self.xmlid_handler.process_document(doc)
+        doc = etree.XML("<root><one xml:id='a'><two xml:id='b'/></one></root>")
+        self.default_handler.process_document(doc, "file.xml")
         result = doc.findall(".//*[@{http://www.w3.org/XML/1998/namespace}id]")
         self.assertEqual(result, [])
+
+    def test_generate_prefix(self):
+        filename = "path/to/file.xml"
+        result = self.prefix_handler.generate_prefix(filename)
+        self.assertEqual(result, "p0cf83b")
+
+    def test_prefix_shifted_if_collision_occurs(self):
+        files = ["path/to/file.xml"] * 100
+        result = [self.prefix_handler.generate_prefix(file) for file in files]
+        self.assertEqual(result, ["p0cf83b"] + [f"p0cf83b{i}" for i in range(99)])
+
+    def test_add_prefix_to_xmlid_value(self):
+        doc = etree.XML("<root><one xml:id='a'><two xml:id='b'/></one></root>")
+        self.prefix_handler.process_document(doc, "file.xml")
+        result = [
+            node.attrib
+            for node in doc.findall(".//*[@{http://www.w3.org/XML/1998/namespace}id]")
+        ]
+        self.assertEqual(
+            result,
+            [
+                {"{http://www.w3.org/XML/1998/namespace}id": "p054536-a"},
+                {"{http://www.w3.org/XML/1998/namespace}id": "p054536-b"},
+            ],
+        )
+
+    def test_prefix_added_to_referencing_attribute(self):
+        doc = etree.XML(
+            """
+        <root>
+            <el1 xml:id='a'/>
+            <el2 corresp='#a'/>
+        </root>
+        """
+        )
+        self.prefix_handler.process_document(doc, "file.xml")
+        result = [node.attrib for node in doc]
+        self.assertEqual(
+            result,
+            [
+                {"{http://www.w3.org/XML/1998/namespace}id": "p054536-a"},
+                {"corresp": "#p054536-a"},
+            ],
+        )
+
+    def test_prefix_not_added_to_other_attributes(self):
+        doc = etree.XML(
+            """
+            <root>
+                <el1 xml:id='a'/>
+                <el2 corresp='#a' other='a'/>
+                <el3 attr='val'/>
+            </root>
+            """
+        )
+        self.prefix_handler.process_document(doc, "file.xml")
+        result = [node.attrib for node in doc]
+        self.assertEqual(
+            result,
+            [
+                {"{http://www.w3.org/XML/1998/namespace}id": "p054536-a"},
+                {"corresp": "#p054536-a", "other": "a"},
+                {"attr": "val"},
+            ],
+        )
+
+    def test_prefix_added_to_attributes_if_other_attributes_before(self):
+        doc = etree.XML(
+            """
+            <root>
+                <el1 xml:id='a'/>
+                <el2 corresp='#a' other='a' prev='#a'/>
+                <el3 attr='val' prev='#a'/>
+            </root>
+            """
+        )
+        self.prefix_handler.process_document(doc, "file.xml")
+        result = [node.attrib for node in doc]
+        self.assertEqual(
+            result,
+            [
+                {"{http://www.w3.org/XML/1998/namespace}id": "p054536-a"},
+                {"corresp": "#p054536-a", "other": "a", "prev": "#p054536-a"},
+                {"attr": "val", "prev": "#p054536-a"},
+            ],
+        )
+
+    def test_prefix_not_added_to_attribute_if_not_exact_match_of_value(self):
+        doc = etree.XML(
+            """
+            <root>
+                <el1 xml:id='a'/>
+                <el2 corresp='#aaa'/>
+                <el3 attr='val' prev='ab#a'/>
+            </root>
+            """
+        )
+        self.prefix_handler.process_document(doc, "file.xml")
+        result = [node.attrib for node in doc]
+        self.assertEqual(
+            result,
+            [
+                {"{http://www.w3.org/XML/1998/namespace}id": "p054536-a"},
+                {"corresp": "#aaa"},
+                {"attr": "val", "prev": "ab#a"},
+            ],
+        )
+
+    def test_prefix_added_to_multiple_referencing_attributes(self):
+        doc = etree.XML(
+            """
+            <root>
+                <el1 xml:id='a'/>
+                <el2 prev='#a' />
+                <el3 sameAs='#a'/>
+                <el4 exclude='#a'/>
+            </root>
+            """
+        )
+        self.prefix_handler.process_document(doc, "file.xml")
+        result = [node.attrib for node in doc]
+        self.assertEqual(
+            result,
+            [
+                {"{http://www.w3.org/XML/1998/namespace}id": "p054536-a"},
+                {"prev": "#p054536-a"},
+                {"sameAs": "#p054536-a"},
+                {"exclude": "#p054536-a"},
+            ],
+        )
+
+    def test_multiple_xmlid_prefixed_and_referenced(self):
+        doc = etree.XML(
+            """
+            <root>
+                <el1 xml:id='a'/>
+                <el2 xml:id='b' prev='#a'/>
+                <el3 xml:id='c' prev='#b'>
+                    <el1 sameAs='#a'/>
+                </el3>
+                <el4 xml:id='d' prev='#c'/>
+            </root>
+            """
+        )
+        self.prefix_handler.process_document(doc, "file.xml")
+        result = [node.attrib for node in doc.iter()]
+        self.assertEqual(
+            result,
+            [
+                {},
+                {"{http://www.w3.org/XML/1998/namespace}id": "p054536-a"},
+                {
+                    "{http://www.w3.org/XML/1998/namespace}id": "p054536-b",
+                    "prev": "#p054536-a",
+                },
+                {
+                    "{http://www.w3.org/XML/1998/namespace}id": "p054536-c",
+                    "prev": "#p054536-b",
+                },
+                {"sameAs": "#p054536-a"},
+                {
+                    "{http://www.w3.org/XML/1998/namespace}id": "p054536-d",
+                    "prev": "#p054536-c",
+                },
+            ],
+        )
+
+    def test_change_prefix_on_root(self):
+        doc = etree.XML("<root xml:id='b'/>")
+        self.prefix_handler.process_document(doc, "file.xml")
+        self.assertEqual(
+            doc.attrib,
+            {"{http://www.w3.org/XML/1998/namespace}id": "p054536-b"},
+        )
+
+    def test_attribute_referencing_root_prefixed(self):
+        doc = etree.XML("<root xml:id='b'><inner prev='#b'/></root>")
+        self.prefix_handler.process_document(doc, "file.xml")
+        result = [node.attrib for node in doc.iter()]
+        self.assertEqual(
+            result,
+            [
+                {"{http://www.w3.org/XML/1998/namespace}id": "p054536-b"},
+                {"prev": "#p054536-b"},
+            ],
+        )
+
+    def test_add_prefix_to_referencing_attribute_on_root(self):
+        doc = etree.XML(
+            """
+            <root next='#a'>
+                <inner xml:id='a'/>
+            </root>
+            """
+        )
+        self.prefix_handler.process_document(doc, "file.xml")
+        result = [node.attrib for node in doc.iter()]
+        self.assertEqual(
+            result,
+            [
+                {"next": "#p054536-a"},
+                {"{http://www.w3.org/XML/1998/namespace}id": "p054536-a"},
+            ],
+        )
